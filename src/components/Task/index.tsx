50eb2ed2ed6bcc5efe099b5fc0bd7870bea8b2cb
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-06-05 01:30:31 -0300

    creating the task form and its amount and completion states

---
+++ src/components/Task/index.tsx
@@ -0,0 +1,24 @@
+import { ITask } from '../../App';
+import styles from './Task.module.css';
+import { TbTrash } from 'react-icons/tb';
+
+interface Props {
+  task: ITask;
+}
+
+export function Task({ task }: Props) {
+  return (
+    <div className={styles.task}>
+      <button className={styles.checkContainer}>
+        <div />
+      </button>
+      <p>
+        {task.title}
+      </p>
+
+      <button className={styles.deleteButton}>
+        <TbTrash size={20} />
+      </button>
+    </div>
+  )
+}
